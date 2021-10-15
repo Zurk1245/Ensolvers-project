5e235fbb8a66ef201c536ff5f3e497a81a797cce
Mariano Mendez <marianito01@live.com>
2021-10-15 10:22:25 -0300

    Created todos controller, users folder and installed MySQL including the ormconfig file

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoController } from './todo/todo/todo.controller';
 
+//import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UsersModule
+  ],
   controllers: [AppController, TodoController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/* @Module({
+  imports: [],
+  controllers: [AppController, TodoController],
+  providers: [AppService],
+})
+export class AppModule {} */
